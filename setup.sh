5f9256132650204f0abc5211b0c0748f13547c1d
BrenoBenevides <66378660+BrenoBenevides@users.noreply.github.com>
2021-03-08 09:37:25 -0300

    Update setup.sh
    Adding my email

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"brenoitalobenevides1@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
